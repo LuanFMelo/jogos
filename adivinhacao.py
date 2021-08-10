b990e3f5c512163fed158f97de2f28a1553c6c69
LuanFMelo <luanfarias@stone.com.br>
2021-08-10 18:12:43 -0300

    feat/while

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,21 +1,31 @@
-# print("*********************************")
-# print("Bem vindo ao jogo de Adivinhação!")
-# print("*********************************")
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
 
-# numero_secreto = 42
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-# chute_str = input("Digite o seu número: ")
-# print("Você digitou: ", chute_str)
 
-# chute = int(chute_str)
+while(rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-# if (numero_secreto == chute):
-#     print("Você acertou!")
-# else:
-#     print("Você errou!")
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-# print("Fim do jogo!")
 
-nome = "Nico"
-sobrenome = "Steppat"
-print(nome, sobrenome, sep="_")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou ! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou ! O seu chute foi menor do que o número secreto.")
+    
+    rodada = rodada +1
+
+print("Fim do jogo!")
