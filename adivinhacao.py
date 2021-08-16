dfaa501d59730456a1750b8dac7a2ae32e2f23eb
LuanFMelo <46304677+LuanFMelo@users.noreply.github.com>
2021-08-15 22:20:18 -0300

    feat/lace for

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,25 +7,27 @@ total_de_tentativas = 3
 rodada = 1
 
 
-while(rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas +1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas)) #string interpolation
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue # Continua com a proxima interacao do for
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
 
-
     if (acertou):
         print("Você acertou!")
+        break # Sai do laço for
     else:
         if(maior):
             print("Você errou ! O seu chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou ! O seu chute foi menor do que o número secreto.")
-    
-    rodada = rodada +1
 
 print("Fim do jogo!")
